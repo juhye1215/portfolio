f8597c245975bf3abb5fb37e164160aedcfce7b8
Juhye <juhye1215@gmail.com>
2021-02-27 19:56:28 -1000

    와이즈돔 업데이트

--- wisedome/static/js/nav-footer.js
+++ wisedome/static/js/nav-footer.js
@@ -3,10 +3,14 @@
 //nav color change
 $(window).scroll(function(){
 	var scroll = $(window).scrollTop();
-	if(scroll > 350){
+	if(scroll > 50){
     $('.fixed-top').css('background', '#e5f1f0');
-    $('.fixed-top').css('opacity', '0.95');
-	} else{
+    $('.fixed-top').css('opacity', '0.99');
+	} else if( scroll ===  0 ){
+		$('.fixed-top').css('background', '#c7e4e2');
+	} 
+	else{
 		$('.fixed-top').css('background', 'transparent');
 	}
 });
+
